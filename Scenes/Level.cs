27c001e8430eabc1ba8332e26405f676e49bd6e4
Anthony Mirabile <amirabil@ad.inria.fr>
2024-06-29 12:09:33 +0200

    mailboxes

---
+++ Scenes/Level.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Level : Node2D
+{
+
+    private List<MailBox> AvailableBoxes;
+    private int currentMailboxScore;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Godot.Collections.Array<Node> mailboxes = GetTree().GetNodesInGroup("Mailbox");
+        foreach (Node item in mailboxes)
+        {
+            RegisterMailbox(item as MailBox);
+        }
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+
+    public void RegisterMailbox(MailBox mailBox)
+    {
+        if (AvailableBoxes == null) AvailableBoxes = new List<MailBox>();
+
+        AvailableBoxes.Add(mailBox);
+        mailBox.OnPlayerEnterZone += (node) => InteractedWithMailbox(mailBox);
+    }
+
+    public void InteractedWithMailbox(MailBox box)
+    {
+        if (AvailableBoxes.Contains(box))
+        {
+            Sprite2D sprite = box.GetNode<Sprite2D>("Sprite2D");
+            sprite.Modulate = new Color(0, 1, 0, 1);
+            AvailableBoxes.Remove(box);
+            if (AvailableBoxes.Count == 0) LevelEnded();
+        }
+    }
+
+
+    public void LevelEnded()
+    {
+        GD.Print("LEVEL ENDED");
+    }
+
+}
